9f1a70c977bda4ba8170870e1af47ccc5a75b5cb
Bechev <bertrand.chevalier@gmail.com>
2018-08-11 11:50:01 -0400

    created migrations for Brands, Categories and Items tables

---
+++ db/migrate/20180811153816_create_items_table.rb
@@ -0,0 +1,16 @@
+class CreateItemsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :color
+      t.float :height
+      t.float :width
+      t.float :length
+      t.string :description
+      t.integer :price
+      t.boolean :status
+      t.integer :category_id
+      t.integer :brand_id
+    end
+  end
+end
