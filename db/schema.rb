8c7dce30157473d88a3d49b0d28608cae58c5195
Bechev <bertrand.chevalier@gmail.com>
2018-08-13 07:32:57 -0400

    added create route for user and basic email validation

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_104908) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
